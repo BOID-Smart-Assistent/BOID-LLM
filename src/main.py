53fc8ec71170a20d6de227aa27f3fb663a61711a
caesaralpha <caesaralpha1994@gmail.com>
2024-11-24 18:32:58 +0100

    change app name and port

--- src/main.py
+++ src/main.py
@@ -1,15 +1,24 @@
 from flask import Flask, request, jsonify
 import llm_chat_system
 
-app = Flask(__name__)
+app = Flask('llmboidapi')
 
-@app.route('/chat', methods=['POST'])
+@app.route('/chat', methods=['GET','POST'])
 def chat():
-    user_input = request.json.get('input')
-    response = llm_chat_system.process_input(user_input)
+    if request.method == 'POST':
+        user_input = request.json.get('input')
+    elif request.method == 'GET':
+        # Retrieve input from query parameter for GET requests
+        user_input = request.args.get('input')
+    try:
+        response = llm_chat_system.process_input(user_input)
+    except:
+        response='ERROR: Please activate the LM studio and select the model'
     return jsonify({'response': response})
 
 if __name__ == '__main__':
-    app.run()
+    print('Running module:{__name__}')
+    app.run(host="0.0.0.0",port=80)
     
-## from gitbash: curl -X POST -H "Content-Type: application/json" -d '{"input": "input: quantum"}' http://localhost:5000/chat
+## from gitbash: curl -X POST -H "Content-Type: application/json" -d '{"input": "input: quantum"}' http://localhost:80/chat
+## from browser: http://localhost:80/chat?input='quantum'
